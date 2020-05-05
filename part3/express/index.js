a52e0249b4edfd9134c52b18a06a682d09062945
Max <max@mproske.com>
2020-05-04 22:06:28 -0700

    Add lesson 3

---
+++ part3/express/index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true,
+  },
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const note = notes.find((note) => note.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter((note) => note.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body
+
+  if (!note) {
+    res.status(400).send('Missing note')
+  }
+
+  // JS witchcraft
+  const id = generateId()
+  const date = new Date()
+
+  notes.concat({ ...note, id, date })
+  res.json(note)
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
